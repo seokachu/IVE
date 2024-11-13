ab0ac95ac65ec8e0ef031a9a248a349cb6bd3077
박서영 <seokachuu@gmail.com>
2024-11-13 21:50:53 +0900

    [Feat] 모바일 페이지 로그인 완료

--- src/components/layout/header/components/MobileUserMenu.tsx
+++ src/components/layout/header/components/MobileUserMenu.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
-import { useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import SignIn from "../../../auth/modal/SignIn";
 import { GoHeart } from "react-icons/go";
 import { SheetClose } from "@/components/ui/sheet";
+import { useRecoilValue } from "recoil";
+import { sessionState } from "@/store";
 
 const MobileUserMenu = () => {
-  const [isLogin] = useState(false);
+  const session = useRecoilValue(sessionState);
 
   return (
     <nav>
@@ -25,24 +26,24 @@ const MobileUserMenu = () => {
           </SheetClose>
         </li>
         <li className="flex-1 hover:bg-zinc-500">
-          {!isLogin ? (
+          {!session ? (
             <SignIn
               title="로그인"
               className="block w-full border p-7 border-dark-gray h-[80px] border-r-0 hover:text-white"
             />
           ) : (
             <SheetClose asChild>
-              <button className="block w-full border p-7 h-[80px] border-dark-gray group border-r-0">
-                <GoHeart
-                  size={28}
-                  className="group-hover:[color:white] m-auto"
-                />
-              </button>
+              <Link
+                href="/mypage"
+                className="block text-center border h-[80px] p-7 border-dark-gray hover:text-white hover:bg-zinc-500 whitespace-nowrap"
+              >
+                마이페이지
+              </Link>
             </SheetClose>
           )}
         </li>
-        <li className="flex-1">
-          {!isLogin ? (
+        {!session && (
+          <li className="flex-1">
             <SheetClose asChild>
               <Link
                 href="/signup"
@@ -51,17 +52,8 @@ const MobileUserMenu = () => {
                 회원가입
               </Link>
             </SheetClose>
-          ) : (
-            <SheetClose asChild>
-              <Link
-                href="/mypage"
-                className="block border h-[80px] p-7 border-dark-gray hover:text-white hover:bg-zinc-500 whitespace-nowrap"
-              >
-                마이페이지
-              </Link>
-            </SheetClose>
-          )}
-        </li>
+          </li>
+        )}
       </ul>
     </nav>
   );
