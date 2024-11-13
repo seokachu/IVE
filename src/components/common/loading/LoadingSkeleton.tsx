f5c360415305aee39cfb7a210b3b27bda5927c90
박서영 <seokachuu@gmail.com>
2024-11-13 23:46:30 +0900

    [Feat] Skeleton 컴포넌트 추가

---
+++ src/components/common/loading/LoadingSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const LoadingSkeleton = () => {
+  return <Skeleton className="w-[100px] h-[20px] rounded-full" />;
+};
+
+export default LoadingSkeleton;
