307d994dcde0721b09b142ddee945d1756a32886
박서영 <seokachuu@gmail.com>
2024-12-01 02:38:31 +0900

    [Feat] 결제금액 스켈레톤 수정

--- src/components/common/loading/CartSummarySkeleton.tsx
+++ src/components/common/loading/CartSummarySkeleton.tsx
@@ -3,22 +3,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 const CartSummarySkeleton = () => {
   return (
     <div className="lg:sticky lg:top-5 flex-1 border rounded-md bg-white shadow-sm p-10 h-fit">
-      <Skeleton className="h-7 w-24 mb-5" />
-      <div className="border-b pb-3 flex justify-between items-center">
-        <Skeleton className="h-5 w-20" />
-        <div className="flex items-center">
-          <Skeleton className="h-7 w-24 mr-1" />
-          <Skeleton className="h-5 w-4" />
+      <div className="mb-12">
+        <Skeleton className="h-7 w-24 mb-5" />
+        <div className="border-b pb-3 flex justify-between items-center">
+          <Skeleton className="h-7 w-20" />
+          <div className="flex items-center">
+            <Skeleton className="h-7 w-24 mr-1" />
+          </div>
         </div>
-      </div>
-      <div className="pt-5 mb-10">
-        <div className="mb-2 flex justify-between items-center">
-          <Skeleton className="h-5 w-16" />
-          <Skeleton className="h-5 w-20" />
-        </div>
-        <div className="flex justify-between items-center">
-          <Skeleton className="h-5 w-20" />
-          <Skeleton className="h-5 w-20" />
+        <div className="pt-5 mb-10">
+          <div className="mb-2 flex justify-between items-center">
+            <Skeleton className="h-5 w-16" />
+            <Skeleton className="h-5 w-20" />
+          </div>
+          <div className="flex justify-between items-center">
+            <Skeleton className="h-5 w-20" />
+            <Skeleton className="h-5 w-20" />
+          </div>
         </div>
       </div>
       <Skeleton className="h-10 w-full rounded-md" />
