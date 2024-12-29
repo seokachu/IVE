13deaeb0bb8e0ea642ff8b30b6378bd643577829
박서영 <seokachuu@gmail.com>
2024-12-29 20:33:26 +0900

    [Feat] GoTopButton유틸 추가

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,4 @@
+import GoTopButton from "@/components/common/button/GoTopButton";
 import ShopContainer from "@/components/shop/ShopContainer";
 
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
       <section className="max-w-[1320px] m-auto px-5 pt-14 pb-28 lg:px-8">
         <ShopContainer />
       </section>
+      <GoTopButton />
     </main>
   );
 };
