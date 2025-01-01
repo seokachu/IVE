365a4ebf1ca05cc3ce527c0a259a7e6c9747946e
박서영 <seokachuu@gmail.com>
2025-01-01 21:43:22 +0900

    [Feat] 로그인 권한분기 컴포넌트 생성

---
+++ src/hooks/AuthGuard.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { sessionState } from "@/store";
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { toast } from "@/hooks/use-toast";
+
+const AuthGuard = ({ children }: { children: React.ReactNode }) => {
+  const session = useRecoilValue(sessionState);
+
+  useEffect(() => {
+    const showToastAndRedirect = () => {
+      if (!session) {
+        Promise.resolve(
+          toast({
+            title: "로그인 후 이용 가능합니다.",
+            duration: 2000,
+          })
+        ).then(() => {
+          setTimeout(() => {
+            window.location.href = "/login";
+          }, 1500);
+          5;
+        });
+      }
+    };
+    setTimeout(showToastAndRedirect, 100);
+  }, [session]);
+
+  if (!session) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthGuard;
