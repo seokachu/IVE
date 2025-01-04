61f7153ae499b4ef068aef692d8a82d518288715
박서영 <seokachuu@gmail.com>
2025-01-04 18:39:39 +0900

    [Feat] Navi 메뉴 pathname 추가 후 폰트사이즈 굵게 적용

--- src/components/layout/header/components/Navigator.tsx
+++ src/components/layout/header/components/Navigator.tsx
@@ -1,14 +1,24 @@
 import type { GnbArrayList } from "@/types";
 import { GNB_ARRAY } from "@/utils/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navigator = () => {
+  const pathname = usePathname();
+
   return (
     <nav>
       <ul className="hidden lg:flex lg:gap-10 lg:items-center">
         {GNB_ARRAY.map((el: GnbArrayList) => (
           <li key={el.label}>
-            <Link href={el.path}>{el.label}</Link>
+            <Link
+              href={el.path}
+              className={`${
+                pathname === el.path ? "font-bold" : "font-normal"
+              }`}
+            >
+              {el.label}
+            </Link>
           </li>
         ))}
       </ul>
