d4f0a1448837154107186a653a2df0933aacc57a
박서영 <seokachuu@gmail.com>
2025-01-06 23:47:31 +0900

    [Fix] 모바일 장바구니 아이콘 클릭시 aside 닫힐 수 있도록 수정

--- src/components/layout/header/components/UserMenu.tsx
+++ src/components/layout/header/components/UserMenu.tsx
@@ -3,6 +3,7 @@ import UserDropdownMenu from "./UserDropdownMenu";
 import { useRecoilValue } from "recoil";
 import { sessionState } from "@/store";
 import SignInModal from "@/components/auth/modal/SignInModal";
+import Link from "next/link";
 
 const UserMenu = () => {
   const session = useRecoilValue(sessionState);
@@ -11,7 +12,9 @@ const UserMenu = () => {
     <nav className="hidden lg:block">
       <ul className="flex gap-4 items-center">
         <li className={`${!session ? "translate-y-0" : "translate-y-[2px]"} `}>
-          <CartIcon className="left-3" />
+          <Link href="/cart">
+            <CartIcon className="left-3" />
+          </Link>
         </li>
         <li className="flex">
           {!session ? <SignInModal /> : <UserDropdownMenu />}
