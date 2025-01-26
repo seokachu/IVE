3965d0f94d469be0752bf3bb5ca450d0259e7957
박서영 <seokachuu@gmail.com>
2025-01-26 20:55:08 +0900

    [Feat] 마이페이지 컴포넌트 메타데이터 추가 후 분리

--- src/metadata/mypage/myPageMetadata.ts
+++ src/metadata/mypage/myPageMetadata.ts
@@ -1,9 +1,9 @@
 export const myPageMetadata = {
   title: "IVE DIVE",
-  description: "IVE 팬페이지 - 소식페이지",
+  description: "IVE 팬페이지 - 마이페이지",
   openGraph: {
     title: "IVE DIVE",
-    description: "IVE 팬페이지 - 소식페이지",
+    description: "IVE 팬페이지 - 마이페이지",
     images: [
       "https://res.cloudinary.com/dknj7kdek/image/upload/v1737888335/og_nb8ueg.png",
     ],
