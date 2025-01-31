b98b7665af074d04cbc91b69c371f7a02c79b56e
박서영 <seokachuu@gmail.com>
2025-02-01 01:15:06 +0900

    [Style] 결제 성공페이지 모바일 반응형 수정

--- src/components/common/loading/PaymentSuccessLoading.tsx
+++ src/components/common/loading/PaymentSuccessLoading.tsx
@@ -1,9 +1,9 @@
 const PaymentSuccessLoading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen sm:min-h-[250px] md:min-h-screen flex items-center justify-center">
       <div className="flex flex-col items-center gap-4">
-        <div>결제 정보를 처리하고 있습니다...</div>
-        <div className="text-sm text-gray-500">잠시만 기다려주세요.</div>
+        <h2>결제 정보를 처리하고 있습니다...</h2>
+        <p className="text-sm text-gray-500">잠시만 기다려주세요.</p>
       </div>
     </div>
   );
