ade519019756071a7d45ee932492811215d29002
박서영 <seokachuu@gmail.com>
2025-01-31 17:21:53 +0900

    [Style] cart page skeleton 모바일 수정

--- src/components/common/loading/CartListLoading.tsx
+++ src/components/common/loading/CartListLoading.tsx
@@ -2,9 +2,9 @@ import { ClipLoader } from "react-spinners";
 
 const CartListLoading = () => {
   return (
-    <div className="flex-[2] p-10 border rounded-md bg-white shadow-sm min-h-[500px] h-fit">
-      <h2 className="font-bold text-xl">장바구니</h2>
-      <div className="w-full min-h-[380px] flex items-center justify-center">
+    <div className="flex-[2] p-5 lg:p-10 border rounded-md bg-white shadow-sm min-h-[500px] h-fit">
+      <h2 className="font-bold text-lg lg:text-xl">장바구니</h2>
+      <div className="w-full min-h-[500px] flex items-center justify-center">
         <ClipLoader />
       </div>
     </div>
