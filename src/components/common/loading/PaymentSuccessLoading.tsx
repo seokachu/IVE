0ed8f10d0da5588b86ea6038f933e2ebedf3c870
박서영 <seokachuu@gmail.com>
2025-02-01 01:26:24 +0900

    [Style] 결제 성공페이지 로딩 수정

--- src/components/common/loading/PaymentSuccessLoading.tsx
+++ src/components/common/loading/PaymentSuccessLoading.tsx
@@ -1,6 +1,6 @@
 const PaymentSuccessLoading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center sm:-mt-20 md:mt-0">
       <div className="flex flex-col items-center gap-4">
         <h2>결제 정보를 처리하고 있습니다...</h2>
         <p className="text-sm text-gray-500">잠시만 기다려주세요.</p>
