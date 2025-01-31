8528de6b5f8c5f2aae543133d88888b3e2d18622
박서영 <seokachuu@gmail.com>
2025-02-01 00:52:13 +0900

    [Style] 기본 체크박스 수정

--- src/components/mypage/address/DefaultAddressCheckbox.tsx
+++ src/components/mypage/address/DefaultAddressCheckbox.tsx
@@ -1,3 +1,4 @@
+import { RHFInput } from "@/components/common/RHFInput";
 import { Label } from "@/components/ui/label";
 import { FaCheck } from "react-icons/fa";
 
@@ -21,15 +22,15 @@ const DefaultAddressCheckbox = ({
 
   return (
     <div className="my-5 flex flex-wrap items-center gap-1">
-      <input
+      <RHFInput
         type="checkbox"
         name="isDefault"
         id="defaultDelivery"
         className="w-4 h-4 translate-y-[1px]"
       />
-      <label htmlFor="defaultDelivery" className="text-sm">
+      <Label htmlFor="defaultDelivery" className="text-sm">
         기본 배송지로 저장
-      </label>
+      </Label>
       <span className="text-gray-500 text-xs">
         &#40;첫 배송지는 자동으로 기본 배송지로 저장됩니다.&#41;
       </span>
