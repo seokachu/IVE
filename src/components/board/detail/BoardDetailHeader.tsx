29cb9a222bde44e6e1840bdf10e39929edc05b41
박서영 <seokachuu@gmail.com>
2025-02-09 22:02:04 +0900

    [Style] 자유게시판 Detail page 모바일 수정

--- src/components/board/detail/BoardDetailHeader.tsx
+++ src/components/board/detail/BoardDetailHeader.tsx
@@ -4,11 +4,11 @@ import { formatDate } from "@/utils/formatDate";
 const BoardDetailHeader = ({ item }: BoardDetailProps) => {
   return (
     <div>
-      <h2 className="text-lg font-bold">{item?.title}</h2>
-      <time className="text-gray-500 text-sm">
+      <h2 className="text-base lg:text-lg font-bold">{item?.title}</h2>
+      <time className="text-gray-500 text-xs lg:text-sm">
         {formatDate(item?.created_at)}
       </time>
-      <span className="ml-2 text-sm text-gray-500">
+      <span className="ml-2 text-xs lg:text-sm text-gray-500">
         조회 {item?.views || 0}
       </span>
     </div>
