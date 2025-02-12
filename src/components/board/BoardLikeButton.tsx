7a9aa28141925c517308c63a27609139fb6a0c53
박서영 <seokachuu@gmail.com>
2025-02-12 20:41:36 +0900

    [Fix] 빌드 에러 수정

--- src/components/board/BoardLikeButton.tsx
+++ src/components/board/BoardLikeButton.tsx
@@ -1,39 +1,40 @@
-import ActionButton from "@/components/common/button/ActionButton";
-import { AiOutlineLike } from "react-icons/ai";
-import { AiFillLike } from "react-icons/ai";
+// import ActionButton from "@/components/common/button/ActionButton";
+// import { AiOutlineLike } from "react-icons/ai";
+// import { AiFillLike } from "react-icons/ai";
 import { sessionState } from "@/store";
 import { useRecoilValue } from "recoil";
-import { toast } from "@/hooks/use-toast";
+// import { toast } from "@/hooks/use-toast";
 import type { BoardDetailProps } from "@/types";
 
 const BoardLikeButton = ({ item }: BoardDetailProps) => {
   const session = useRecoilValue(sessionState);
   const userId = session?.user?.id;
-
+  console.log(item);
+  console.log(userId);
   //좋아요 상태 query
   // const { data: isLiked } = useLikeStatus(item.id, userId);
   // const { mutate: toggleLike, isPending } = useToggleLike(item.id, userId);
 
-  const handleToggleLikeClick = () => {
-    if (!session) {
-      toast({
-        title: "로그인이 필요합니다.",
-        description: "로그인 페이지로 이동하여 로그인 해주세요.",
-        variant: "destructive",
-      });
-      return;
-    }
+  // const handleToggleLikeClick = () => {
+  //   if (!session) {
+  //     toast({
+  //       title: "로그인이 필요합니다.",
+  //       description: "로그인 페이지로 이동하여 로그인 해주세요.",
+  //       variant: "destructive",
+  //     });
+  //     return;
+  //   }
 
-    // toggleLike(undefined, {
-    //   onSuccess: (newStatus) => {
-    //     toast({
-    //       title: newStatus
-    //         ? "좋아요를 눌렀습니다."
-    //         : "좋아요가 취소되었습니다.",
-    //     });
-    //   },
-    // });
-  };
+  // toggleLike(undefined, {
+  //   onSuccess: (newStatus) => {
+  //     toast({
+  //       title: newStatus
+  //         ? "좋아요를 눌렀습니다."
+  //         : "좋아요가 취소되었습니다.",
+  //     });
+  //   },
+  // });
+  // };
 
   return (
     <div className="flex items-center justify-center mb-5">
