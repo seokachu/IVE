25c0702265d70430dc4e23698611d4020eb7e19e
박서영 <seokachuu@gmail.com>
2025-03-06 16:07:45 +0900

    [Style] 메인페이지 screen 반응형 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ export const metadata = loginMetadata;
 
 const page = () => {
   return (
-    <main className="w-full h-screen flex items-center justify-center -mt-[70px] lg:mt-0">
+    <main className="w-full h-[100dvh] lg:h-screen flex items-center justify-center -mt-[70px] lg:mt-0">
       <SignIn />
     </main>
   );
