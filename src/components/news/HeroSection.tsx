fcad6b6a426066335b20c6760533276420f5e9f1
박서영 <seokachuu@gmail.com>
2025-03-10 19:29:31 +0900

    [Fix] 소식페이지 Button 컴포넌트 분리

--- src/components/news/HeroSection.tsx
+++ src/components/news/HeroSection.tsx
@@ -1,15 +1,7 @@
-"use client";
-import ActionButton from "../common/button/ActionButton";
+import ScrollToSectionButton from "./ScrollToSectionButton";
 
 const HeroSection = () => {
   const textStyle = "[text-shadow:_1px_2px_5px_rgb(0_0_0_/_0.5)]";
-
-  const scrollToSection = (id: string) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <section className="relative h-[100dvh] lg:h-screen flex flex-col gap-2 items-center justify-center bg-main-image bg-cover bg-center bg-no-repeat">
@@ -25,23 +17,7 @@ const HeroSection = () => {
             아이브와 함께하는 특별한 순간
           </h2>
         </div>
-        <div className="flex items-center gap-5 flex-col lg:flex-row">
-          <ActionButton
-            onClick={() => scrollToSection("news_section")}
-            variant="primary"
-            className="px-8 py-4 !rounded-full font-bold"
-          >
-            Latest News
-          </ActionButton>
-
-          <ActionButton
-            onClick={() => scrollToSection("gallery_section")}
-            variant="default"
-            className="px-8 py-4 !rounded-full bg-white font-bold"
-          >
-            View Gallery
-          </ActionButton>
-        </div>
+        <ScrollToSectionButton />
       </div>
     </section>
   );
