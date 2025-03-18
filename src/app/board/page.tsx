d8b68dba686a00ec2434574e9ae299cf140d9685
박서영 <seokachuu@gmail.com>
2025-03-19 01:48:22 +0900

    [Fix] 빌드 에러 수정

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import { boardMetadata } from "@/metadata/board/boardMetadata";
 import BoardContainer from "@/components/board/BoardContainer";
-
+import BoardSkeleton from "@/components/common/loading/BoardSkeleton";
 export const metadata = boardMetadata;
 
 const page = () => {
   return (
     <main className="w-full min-h-screen">
       <section className="max-w-[1320px] m-auto pt-14 pb-28 lg:px-8">
-        <BoardContainer />
+        <Suspense fallback={<BoardSkeleton />}>
+          <BoardContainer />
+        </Suspense>
       </section>
     </main>
   );
