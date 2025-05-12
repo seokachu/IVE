bddb7c587f61b2d5280502548f312dd723019086
Seoyoung Park <seokachuu@gmail.com>
2025-05-12 23:23:01 +0900

    [Fix] 소식 페이지 프리페칭 추가

--- src/components/main/AlbumList.tsx
+++ src/components/main/AlbumList.tsx
@@ -44,7 +44,11 @@ const AlbumList = () => {
       wrapperTag="ul"
     >
       {albums?.map((album) => (
-        <SwiperSlide key={album.title} tag="li" className="bg-cover h-fit bg-center lg:!w-max lg:!h-full lg:pr-5">
+        <SwiperSlide
+          key={album.title}
+          tag="li"
+          className="bg-cover h-fit bg-center lg:!w-max lg:!h-full lg:pr-5"
+        >
           <AlbumListItem album={album} />
         </SwiperSlide>
       ))}
